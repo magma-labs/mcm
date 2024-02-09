8fbc926e0479c3511397dc8849c920d1377c500f
Kevin Perez <kevin.perez@magmalabs.io>
2024-02-08 20:02:44 -0600

    Fix Rails version in migrations
    Should be 5.2 instead of the most recent version.

--- db/migrate/20240122200047_create_mcm_assets.rb
+++ db/migrate/20240122200047_create_mcm_assets.rb
@@ -1,4 +1,4 @@
-class CreateMcmAssets < ActiveRecord::Migration[7.1]
+class CreateMcmAssets < ActiveRecord::Migration[5.2]
   def change
     create_table :mcm_assets, if_not_exists: true do |t|
       t.belongs_to :components_page
